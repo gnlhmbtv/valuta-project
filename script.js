35bc5ff306a3866e37a43addd45fd833e358bf39
gnlhmbtv <guneljh@code.edu.az>
2022-11-20 12:03:35 +0400

    refactor

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ let rightInput = document.querySelector("#right-input");
 let base;
 let convertTo;
 
-
 function convertLeft() {
   let str;
   let leftvalue = leftInput.value;
@@ -13,8 +12,7 @@ function convertLeft() {
     var style = window
       .getComputedStyle(e, "")
       .getPropertyValue("background-color");
-    if (style == "rgb(131, 58, 224)") {
-      // console.log(e, 'hello');
+    if (e.classList.contains("selected")) {
       base = e.innerHTML;
       console.log(base, leftvalue);
     }
@@ -24,21 +22,25 @@ function convertLeft() {
     var style = window
       .getComputedStyle(e, "")
       .getPropertyValue("background-color");
-    if (style == "rgb(131, 58, 224)") {
-      // console.log(e, 'hello');
+
+    if (e.classList.contains("selected")) {
       convertTo = e.innerHTML;
-      console.log(base, leftvalue);
     }
   });
 
-  fetch(`https://api.exchangerate.host/latest?base=${base}&symbols=${convertTo}`)
-        .then((res) => res.json())
-        .then((data) => {
-          rightInput?.setAttribute("value", Object.values(data.rates)[0] * leftvalue);
-        });
+  fetch(
+    `https://api.exchangerate.host/latest?base=${base}&symbols=${convertTo}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      rightInput?.setAttribute(
+        "value",
+        Object.values(data.rates)[0] * leftvalue
+      );
+    });
 }
 
-function convertRight(){
+function convertRight() {
   let str;
   let rightvalue = rightInput.value;
 
@@ -46,8 +48,7 @@ function convertRight(){
     var style = window
       .getComputedStyle(e, "")
       .getPropertyValue("background-color");
-    if (style == "rgb(131, 58, 224)") {
-      // console.log(e, 'hello');
+    if (e.classList.contains("selected")) {
       base = e.innerHTML;
       console.log(base, rightvalue);
     }
@@ -57,107 +58,32 @@ function convertRight(){
     var style = window
       .getComputedStyle(e, "")
       .getPropertyValue("background-color");
-    if (style == "rgb(131, 58, 224)") {
-      // console.log(e, 'hello');
+    if (e.classList.contains("selected")) {
       convertTo = e.innerHTML;
       console.log(base, rightvalue);
     }
   });
 
- 
-
-  fetch(`https://api.exchangerate.host/latest?base=${base}&symbols=${convertTo}`)
-        .then((res) => res.json())
-        .then((data) => {
-          leftInput?.setAttribute("value", Object.values(data.rates)[0] * rightvalue);
-        });
+  fetch(
+    `https://api.exchangerate.host/latest?base=${base}&symbols=${convertTo}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      leftInput?.setAttribute(
+        "value",
+        Object.values(data.rates)[0] * rightvalue
+      );
+    });
 }
 
-function changeColorleft(str) {
-  let b1 = document.querySelector(".button1-left");
-  let b2 = document.querySelector(".button2-left");
-  let b3 = document.querySelector(".button3-left");
-  let b4 = document.querySelector(".button4-left");
+function select(current) {
+  const parentNode = current.parentNode;
+  const allButtons = document.querySelectorAll(
+    `.${parentNode.className} button`
+  );
 
-  if (str == "one") {
-    b1.style.backgroundColor = "#833ae0";
-    b2.style.backgroundColor = "white";
-    b3.style.backgroundColor = "white";
-    b4.style.backgroundColor = "white";
-    b1.style.color = "white";
-    b2.style.color = "black";
-    b3.style.color = "black";
-    b4.style.color = "black";
-  } else if (str == "two") {
-    b2.style.backgroundColor = "#833ae0";
-    b1.style.backgroundColor = "white";
-    b3.style.backgroundColor = "white";
-    b4.style.backgroundColor = "white";
-    b2.style.color = "white";
-    b1.style.color = "black";
-    b3.style.color = "black";
-    b4.style.color = "black";
-  } else if (str == "three") {
-    b3.style.backgroundColor = "#833ae0";
-    b1.style.backgroundColor = "white";
-    b2.style.backgroundColor = "white";
-    b4.style.backgroundColor = "white";
-    b1.style.color = "black";
-    b3.style.color = "white";
-    b2.style.color = "black";
-    b4.style.color = "black";
-  } else if (str == "four") {
-    b4.style.backgroundColor = "#833ae0";
-    b1.style.backgroundColor = "white";
-    b2.style.backgroundColor = "white";
-    b3.style.backgroundColor = "white";
-    b1.style.color = "black";
-    b4.style.color = "white";
-    b2.style.color = "black";
-    b3.style.color = "black";
-  }
-}
-function changeColorright(str) {
-  let b1 = document.querySelector(".button1-right");
-  let b2 = document.querySelector(".button2-right");
-  let b3 = document.querySelector(".button3-right");
-  let b4 = document.querySelector(".button4-right");
-
-  if (str == "one") {
-    b1.style.backgroundColor = "#833ae0";
-    b2.style.backgroundColor = "white";
-    b3.style.backgroundColor = "white";
-    b4.style.backgroundColor = "white";
-    b1.style.color = "white";
-    b2.style.color = "black";
-    b3.style.color = "black";
-    b4.style.color = "black";
-  } else if (str == "two") {
-    b2.style.backgroundColor = "#833ae0";
-    b1.style.backgroundColor = "white";
-    b3.style.backgroundColor = "white";
-    b4.style.backgroundColor = "white";
-    b2.style.color = "white";
-    b1.style.color = "black";
-    b3.style.color = "black";
-    b4.style.color = "black";
-  } else if (str == "three") {
-    b3.style.backgroundColor = "#833ae0";
-    b1.style.backgroundColor = "white";
-    b2.style.backgroundColor = "white";
-    b4.style.backgroundColor = "white";
-    b1.style.color = "black";
-    b3.style.color = "white";
-    b2.style.color = "black";
-    b4.style.color = "black";
-  } else if (str == "four") {
-    b4.style.backgroundColor = "#833ae0";
-    b1.style.backgroundColor = "white";
-    b2.style.backgroundColor = "white";
-    b3.style.backgroundColor = "white";
-    b1.style.color = "black";
-    b4.style.color = "white";
-    b2.style.color = "black";
-    b3.style.color = "black";
-  }
+  allButtons.forEach(function resetButton(button) {
+    button.classList.remove("selected");
+  });
+  current.classList.add("selected");
 }
